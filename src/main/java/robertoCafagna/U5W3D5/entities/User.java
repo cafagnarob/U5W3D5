e13f1015d1e524971efc3219358c2561aa2be9db
cafagnarob <cafagna.rob@gmail.com>
2026-07-17 13:57:36 +0200

    create security

--- src/main/java/robertoCafagna/U5W3D5/entities/User.java
+++ src/main/java/robertoCafagna/U5W3D5/entities/User.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import robertoCafagna.U5W3D5.Enum.Ruolo;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,7 +20,7 @@ import robertoCafagna.U5W3D5.Enum.Ruolo;
 @ToString
 @Table(name = "users")
 @JsonIgnoreProperties({"password"})
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,5 +50,15 @@ public class User {
         this.email = email;
         this.password = password;
         this.ruolo = Ruolo.USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.ruolo.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 }
---
+++ src/main/java/robertoCafagna/U5W3D5/security/TokenFilter.java
@@ -0,0 +1,58 @@
+package robertoCafagna.U5W3D5.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import robertoCafagna.U5W3D5.entities.User;
+import robertoCafagna.U5W3D5.exceptions.UnauthorizedException;
+import robertoCafagna.U5W3D5.services.UserService;
+
+import java.io.IOException;
+
+@Component
+public class TokenFilter extends OncePerRequestFilter {
+    private final JWTTools jwtTools;
+    private final UserService userService;
+
+    public TokenFilter(JWTTools jwtTools, UserService userService) {
+        this.jwtTools = jwtTools;
+        this.userService = userService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new UnauthorizedException("inserire il token del giusto formato");
+
+        String accessToken = authHeader.replace("Bearer ", "");
+
+        this.jwtTools.verifyToken(accessToken);
+
+
+        //1.
+        Long userId = this.jwtTools.extractIdFromToken(accessToken);
+        User found = this.userService.findById(userId);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(found, null,
+                found.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+
+    }
+}
