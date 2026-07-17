07ea142017a9354d668ccab555db6f20e26cb880
cafagnarob <cafagna.rob@gmail.com>
2026-07-17 10:37:49 +0200

    Create BadRequestException.java

---
+++ src/main/java/robertoCafagna/U5W3D5/exceptions/BadRequestException.java
@@ -0,0 +1,8 @@
+package robertoCafagna.U5W3D5.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
