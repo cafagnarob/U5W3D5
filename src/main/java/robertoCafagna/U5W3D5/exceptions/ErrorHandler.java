d948ee7756d0b037e4077c85803da25eab683ca4
cafagnarob <cafagna.rob@gmail.com>
2026-07-17 10:48:22 +0200

    Create ErrorHandler.java

---
+++ src/main/java/robertoCafagna/U5W3D5/exceptions/ErrorHandler.java
@@ -0,0 +1,45 @@
+package robertoCafagna.U5W3D5.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import robertoCafagna.U5W3D5.DTO.ErrorDTO;
+import robertoCafagna.U5W3D5.DTO.ErrorListDTO;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorListDTO handleValidation(ValidationException ex) {
+        return new ErrorListDTO(ex.getMessage(), LocalDateTime.now(), ex.getErrorsList());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDTO handleBadRequest(BadRequestException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorDTO handleNotFound(NotFoundException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorDTO handleGeneralException(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorDTO("ERRORE INTRENO AL SERVER", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorDTO handleUnauthorized(UnauthorizedException ex) {
+        return new ErrorDTO(ex.getMessage(), LocalDateTime.now());
+    }
+}
