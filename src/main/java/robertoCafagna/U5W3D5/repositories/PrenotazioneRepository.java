63819fb1b285bbaf6142b225af254341ff40ddb0
cafagnarob <cafagna.rob@gmail.com>
2026-07-17 10:02:45 +0200

    Create PrenotazioneRepository.java

---
+++ src/main/java/robertoCafagna/U5W3D5/repositories/PrenotazioneRepository.java
@@ -0,0 +1,14 @@
+package robertoCafagna.U5W3D5.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import robertoCafagna.U5W3D5.entities.Prenotazione;
+
+import java.util.List;
+
+public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+    boolean existsByUtenteIdAndEventoId(Long userId, Long eventoId);
+
+    List<Prenotazione> findByUtenteId(Long id);
+    
+    void deleteByUtenteIdAndEventoId(Long userId, Long eventoId);
+}
